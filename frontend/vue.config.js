7b3afe2621e2f33b97ed9b5457ee0a5a689777ba
André Gonçalves <andregoncalves1@gmail.com>
2024-12-29 00:21:31 +0000

    [FEAT] First version

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -4,5 +4,14 @@ module.exports = {
     devServer: {
       port: 8081,
     },
+    css: {
+        loaderOptions: {
+          sass: {
+            additionalData: `
+              @use "vuetify/styles" as *;
+            `,
+          },
+        },
+    },
   }
   
